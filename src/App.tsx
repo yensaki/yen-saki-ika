1907737ff35af851bb5eb42cbc891088865a4cc1
yensaki <mov.an.double@gmail.com>
2020-09-22 22:42:03 +0900

    Strictly switch link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import RootIndex from './pages/index'
 import WeaponsIndex from './pages/weapons/index'
 import About from './pages/about'
@@ -10,10 +10,12 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Route exact path="/" component={RootIndex} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/weapons" component={WeaponsIndex} />
         <Navbar />
+        <Switch>
+          <Route exact path="/" component={RootIndex} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/weapons" component={WeaponsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
