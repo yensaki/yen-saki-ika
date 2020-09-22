177a1627e9e5101f7709d3431af25f425decaa82
yensaki <mov.an.double@gmail.com>
2020-09-22 21:59:34 +0900

    Display weapons list

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,6 @@ function App() {
         <Route exact path="/" component={RootIndex} />
         <Route exact path="/about" component={About} />
         <Route exact path="/weapons" component={WeaponsIndex} />
-
-        <header className="App-header">
-          <Navbar />
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
       </div>
     </BrowserRouter>
   );
--- src/pages/weapons/index.tsx
+++ src/pages/weapons/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
+type Weapon = {
+    id: number,
+    name: string
+}
+
 function WeaponsIndex() {
+    const weapons: Weapon[] = [{id: 1, name: "わかばシューター"}]
+
     return (
         <div>
-        <h1>Weapons index!!</h1>
+            <h1>Weapons index!!</h1>
+
+            <div>
+                {weapons.map((weapon) => (
+                    <li key={weapon.id}>{weapon.name}</li>
+                ))}
+            </div>
         </div>
     )
 }
